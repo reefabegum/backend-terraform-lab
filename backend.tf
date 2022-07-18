bdb1af139b9f78888d02aa3d19754147a0c5e2bf
reefabegum <reefa.begum@cloudreach.com>
2022-07-18 19:07:19 +0530

    hello

--- backend.tf
+++ backend.tf
@@ -2,5 +2,7 @@ terraform {
     backend "s3" {
         bucket = "talent-academy-reefa-lab"
         key = "talent-academy/backend/terraform.tfstates"
+        region = "eu-west-1"
+        dynamodb_table = "terraform-lock"
     }
 }
--- main.tf
+++ main.tf
@@ -29,4 +29,20 @@ resource "aws_s3_bucket" "storage_bucket" {
     }
 }
 
+resource "aws_dynamodb_table" "terraform_lock_tbl" {
+  name           = "terraform-lock"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+    attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags           = {
+    Name = "terraform-lock"
+  }
+}
+
 
