5c57631c3ff3348800a0ba9e28be6c970bcbbfb6
reefabegum <reefa.begum@cloudreach.com>
2022-07-20 13:47:11 +0530

    add the policies

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
-    backend "s3" {
-        bucket = "talent-academy-reefa-lab"
-        key = "talent-academy/backend/terraform.tfstates"
-        region = "eu-west-1"
-        dynamodb_table = "terraform-lock"
-    }
+  backend "s3" {
+    bucket         = "talent-academy-reefa-lab"
+    key            = "talent-academy/backend/terraform.tfstates"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-lock"
+  }
 }
--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 resource "aws_s3_bucket" "state_file_bucket" {
-    bucket = "talent-academy-reefa-lab"
-    
-    tags = {
-        Name = "talent-academy-reefa-lab"
-        Environment = "Lab"
-    }
-    lifecycle {
-        prevent_destroy = true
-    }
+  bucket = "talent-academy-reefa-lab"
+
+  tags = {
+    Name        = "talent-academy-reefa-lab"
+    Environment = "Lab"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_s3_bucket_versioning" "version_my_bucket" {
-    bucket = aws_s3_bucket.state_file_bucket.id
-    versioning_configuration {
-        status = "Enabled"
-    }
+  bucket = aws_s3_bucket.state_file_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket" "storage_bucket" {
-    bucket = "talent-academy-reefa1-lab"
-    
-    tags = {
-        Name = "talent-academy-reefa1-lab"
-        Environment = "Lab"
-    }
-    lifecycle {
-        prevent_destroy = true
-    }
+  bucket = "talent-academy-reefa1-lab"
+
+  tags = {
+    Name        = "talent-academy-reefa1-lab"
+    Environment = "Lab"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_dynamodb_table" "terraform_lock_tbl" {
@@ -35,12 +35,12 @@ resource "aws_dynamodb_table" "terraform_lock_tbl" {
   write_capacity = 1
   hash_key       = "LockID"
 
-    attribute {
+  attribute {
     name = "LockID"
     type = "S"
   }
 
-  tags           = {
+  tags = {
     Name = "terraform-lock"
   }
 }
