934faf07eb7b3a9f8f7b7b4f2fc9e56e85c020d8
reefabegum <reefa.begum@cloudreach.com>
2022-07-20 18:51:25 +0530

    added again

--- role.tf
+++ role.tf
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "github_assume_role_policy" {
             identifiers = [aws_iam_openid_connect_provider.default.arn]
         }
         condition {
-            test ="StringLike"
+            test ="StringEquals"
             variable = "token.actions.githubusercontent.com:aud"
             values = ["sts.amazonaws.com"]
         }
